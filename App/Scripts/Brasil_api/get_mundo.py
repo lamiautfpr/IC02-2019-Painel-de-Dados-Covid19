26c2b03ade8445138ceec978b5877dbf5cdb53c6
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-05-14 17:15:03 -0300

    br_api mundo verifyin' updated data

--- App/Scripts/Brasil_api/get_mundo.py
+++ App/Scripts/Brasil_api/get_mundo.py
@@ -1,35 +1,40 @@
-from Scripts.functions import urlGenerator, getApi
+from Scripts.functions import now, urlGenerator, getApi, formatDate
 from DataBase import sqlCreator
 
 def insertData(session):
 
-    insertObj = sqlCreator.Insert(session)
     selectObj = sqlCreator.Select(session)
-    
-    date = ''
+    date = selectObj.LastDate('updated_at', '"Brasil_api_base_mundo"')
 
-    url = urlGenerator(4, date)
-    res = getApi(url)
+    day = now()
 
-    result = res.get('data')
+    if formatDate(2, date) != formatDate(2, day):
+        url = urlGenerator(4, '')
+        res = getApi(url)
 
-    for row in result:
-        country = row.get('country')
-        cases = row.get('cases')
-        confirmed = row.get('confirmed')
-        deaths = row.get('deaths')
-        recovered = row.get('recovered')
-        updated_at = row.get('updated_at')
+        result = res.get('data')
+
+        insertObj = sqlCreator.Insert(session)
+
+        for row in result:
+            country = row.get('country')
+            cases = row.get('cases')
+            confirmed = row.get('confirmed')
+            deaths = row.get('deaths')
+            recovered = row.get('recovered')
+            updated_at = row.get('updated_at')
+            
+            listdate = [
+                country,
+                cases,
+                confirmed,
+                deaths,
+                recovered,
+                updated_at
+            ]
         
-        listdate = [
-            country,
-            cases,
-            confirmed,
-            deaths,
-            recovered,
-            updated_at
-        ]
-    
-        insertObj.Brasilapi_mundo(listdate)
+            insertObj.Brasilapi_mundo(listdate)
+    else:
+        print("Updated data are up to date.")
 
     return ''
