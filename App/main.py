3bcd10750e20fae1ecb5b1f4bb80fa552bd1c152
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-05-19 14:33:25 -0300

    get_mundo_hdx working

--- App/main.py
+++ App/main.py
@@ -1,5 +1,6 @@
 # from Scripts.Brasil_io import get_brasilio, get_cartorio
 from Scripts.Brasil_api import get_brasilapi, get_mundo
+from Scripts.HDX import get_mundo_hdx
 from DataBase.engineCreator import engineDb
 from sqlalchemy.orm import sessionmaker
 
@@ -16,8 +17,11 @@ session = Session()
 
 # BR.api
 # Brasil.apiNacional
-get_brasilapi.insertData(session)
+# get_brasilapi.insertData(session)
 # Brasil.apiMundial
-get_mundo.insertData(session)
+# get_mundo.insertData(session)
+
+# HDX 
+get_mundo_hdx
 
 session.Close()
