5d6137a254cd6e6a9aca0feb599d7719b0d7ec0a
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-05-20 23:58:55 -0300

    wcota full

--- App/Scripts/WCota/get_wcota_nacional.py
+++ App/Scripts/WCota/get_wcota_nacional.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def cleaner(dataset):
 
-    dataset = dataset.sort_values(by='Date', ascending=False)
+    dataset = dataset.sort_values(by='date', ascending=False)
 
     dataset.reset_index(drop=True, inplace=True)
 
@@ -12,19 +12,21 @@ def cleaner(dataset):
 def catcher():
 
     url = ("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities.csv")
-    dataset = pd.read_csv(url, encoding='utf-8', engine='python', error_bad_lines=False, usecols=["date", "state", "city", "deaths", "totalCases"])
-    dataset.rename(columns={'date': 'Date', 'state': 'State', 'city': 'City', 'deaths': 'Deaths'}, inplace=True)
-
+    # dataset = pd.read_csv(url, encoding='utf-8', engine='python', error_bad_lines=False, usecols=["date", "state", "city", "deaths", "totalCases"])
+    # dataset.rename(columns={'date': 'Date', 'state': 'State', 'city': 'City', 'deaths': 'Deaths'}, inplace=True)
+    dataset = pd.read_csv(url, encoding='utf-8', engine='python', error_bad_lines=False)
+    
     dataset = cleaner(dataset)
 
     return dataset
 
 def insertData(session):
 
-    dbFormat = tableClass.WCota_nacional()
+    # dbFormat = tableClass.WCota_nacional()
 
     dataset = catcher()
     
-    dataset.to_sql('WCota_base_nacional', con=session.get_bind(), index_label='id', if_exists='replace', method='multi', dtype=dbFormat)
+    # dataset.to_sql('WCota_base_nacional', con=session.get_bind(), index_label='id', if_exists='replace', method='multi', dtype=dbFormat)
+    dataset.to_sql('WCota_base_nacional', con=session.get_bind(), index_label='id', if_exists='replace', method='multi')
 
     return ''
--- App/main.py
+++ App/main.py
@@ -12,21 +12,21 @@ session = Session()
 
 # BR.io
 # Brasil.io Dados Nacionais
-get_brasil.insertData(session)
+#get_brasil.insertData(session)
 # Brasil.io Dados Cartório
-get_cartorio.insertData(session)
+#get_cartorio.insertData(session)
 
 
 # BR.api
 # Brasil.apiNacional
-get_brasilapi.insertData(session)
+#get_brasilapi.insertData(session)
 # Brasil.apiMundial
-get_mundo.insertData(session)
+#get_mundo.insertData(session)
 
 
 # HDX
 # HDX.getMundo
-get_mundo_hdx.insertData(session)
+#get_mundo_hdx.insertData(session)
 
 
 # WCota
