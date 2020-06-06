6c3fb40ec86550e80719db07d572371396311b10
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-06-06 00:00:56 -0300

    SESA_base_pdf

---
+++ App/Scripts/SESA/get_base_pdf.py
@@ -0,0 +1,36 @@
+from Scripts.functions import now
+from datetime import datetime
+from DataBase import tableClass
+import pandas as pd
+
+def catcher():
+
+    gid = ['1593460334', '618041857', '1431072159', '1317012264', '1297484146', '354728218', '355601818', '1342035615']
+
+    listDatasets = []
+
+    i=0
+    while i < len(gid):
+        url = ("https://docs.google.com/spreadsheets/d/1mw17ZXJaRML5QKcZPACVE-j7gJoqyv-TnOyG5ZCKINM/export?gid={}&format=csv".format(gid[i]))
+        dataset = pd.read_csv(url, encoding='utf-8', engine='python', error_bad_lines=False)
+        
+        listDatasets.append(dataset)
+        
+        i+=1
+
+    return listDatasets
+
+def insertData(session):
+
+    print("Coletando e inserindo dados para SESA-base-PDF..")
+
+    listDatasets = catcher()
+    listTitles = ['dadosGerais', 'faixaEtaria', 'evoluConfirmados', 'examesRT', 'leitosMacrorregiao', 'casosSRAG', 'comorbidadesObitos', 'obitosCor']
+
+    i=0
+    while i < len(listDatasets):
+        listDatasets[i].to_sql('SESA_base_{}'.format(listTitles[i]), con=session.get_bind(), index_label='id', if_exists='replace', method='multi', chunksize=50000)
+        
+        i+=1
+
+    return ''
--- App/insertData.py
+++ App/insertData.py
@@ -3,7 +3,7 @@ from Scripts.Brasil_api import get_brasilapi, get_mundo
 from Scripts.HDX import get_mundo_hdx
 from Scripts.WCota import get_wcota_nacional, get_wcota_leitos, get_wcota_suspects
 from Scripts.SRAG_Covid_Brasil import get_srag
-from Scripts.SESA import get_base_parana
+from Scripts.SESA import get_base_parana, get_base_pdf
 from DataBase.engineCreator import engineDb
 from sqlalchemy.orm import sessionmaker
 
@@ -19,28 +19,30 @@ session = Session()
 
 # BR.api
 # Brasil.apiNacional
-# get_brasilapi.insertData(session)
+get_brasilapi.insertData(session)
 # Brasil.apiMundial
-# get_mundo.insertData(session)
+get_mundo.insertData(session)
 
 # HDX
 # HDX.getMundo
-# get_mundo_hdx.insertData(session)
+get_mundo_hdx.insertData(session)
 
 # WCota
 # WCota.getNacional
-# get_wcota_nacional.insertData(session)
+get_wcota_nacional.insertData(session)
 # WCota.getLeitos
-# get_wcota_leitos.insertData(session)
+get_wcota_leitos.insertData(session)
 # WCota.getSuspects
-# get_wcota_suspects.insertData(session)
+get_wcota_suspects.insertData(session)
 
 # Covid Brazil
 # SRAG
-# get_srag.insertData(session)
+get_srag.insertData(session)
 
 # SESA Paraná
+# CSV
 get_base_parana.insertData(session)
-
+# PDF
+get_base_pdf.insertData(session)
 
 session.Close()
