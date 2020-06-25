c0e2b6a6131e80d2a90ec1c5bc0a00826ffcde97
HugoJFreitas <hugojtdfreitas@gmail.com>
2020-06-25 14:24:32 -0300

    Dia 06-05 ate 24-06
    Dia 24 englobado na coleta

--- App/test.py
+++ App/test.py
@@ -81,41 +81,58 @@ def transform(dfs):
                 df[d] = df[d].str.replace(".", "").str.replace("%", "").astype(int)
             except:
                 pass
-    # for sheet in list_sheets:
-    #     for df in dfs[sheet]:
-    #         try:
-    #             dfs[sheet][df] = dfs[sheet][df].str.replace(".", "").astype(int)
-    #         except:
-    #             pass 
         
     return dfs
         
 def cleanner(dfs):
 
     if len(dfs) == 2:
-        # TRATANDO OCUPAÇÕES
         ocupacao = dfs[0]
-        ocupacao = pd.concat([ocupacao[4:6], ocupacao[7:8]]).astype(str).values.tolist()
-        new_ocupacao = [] # stack de new lines
 
-        for ocup in ocupacao:
-            new_line = [] # stack line
-            for oo in ocup:
-                if len(oo) > 5:
-                    oo = oo.split(' ')
-                    for o in oo:
-                        new_line.append(o)
-                else:
-                    new_line.append(oo)
-            new_ocupacao.append(new_line)
+        if len(ocupacao.dropna()) == 3:
+            ocupacao.dropna(inplace=True)
+            ocupacao[0] = ''
+            ocupacao = ocupacao.astype(str).values.tolist()
 
-        ocupacao = pd.DataFrame(new_ocupacao)
+            new_ocupacao = [] # stack de new lines
+            for ocup in ocupacao:
+                new_line = [] # stack line
+                for oo in ocup:
+                    if len(oo) > 5:
+                        oo = oo.split(' ')
+                        for o in oo:
+                            new_line.append(o)
+                    else:
+                        new_line.append(oo)
+                new_ocupacao.append(new_line)
+
+            ocupacao = pd.DataFrame(new_ocupacao)
+            ocupacao.iloc[0][0] = "UTI"
+            ocupacao.iloc[1][0] = "CLINICO"
+            ocupacao.iloc[-1][0] = "UTI E CLINICO"
+            
+            # ocupacao.columns = ocupacao_columns
+            # ocupacao.drop(columns=['sus total', 'priv total', 'total susp', 'total conf', 'total total'], inplace=True)
+        else:
+            ocupacao = pd.concat([ocupacao[4:6], ocupacao[7:8]]).astype(str).values.tolist()
+            new_ocupacao = [] # stack de new lines
+
+            for ocup in ocupacao:
+                new_line = [] # stack line
+                for oo in ocup:
+                    if len(oo) > 5:
+                        oo = oo.split(' ')
+                        for o in oo:
+                            new_line.append(o)
+                    else:
+                        new_line.append(oo)
+                new_ocupacao.append(new_line)
+
+            ocupacao = pd.DataFrame(new_ocupacao)
+            ocupacao.iloc[-1][0] = "UTI E CLINICO"
+
         ocupacao.columns = ocupacao_columns
-        ocupacao.iloc[-1][0] = "UTI E CLINICO"
         ocupacao.drop(columns=['sus total', 'priv total', 'total susp', 'total conf', 'total total'], inplace=True)
-
-        # print(ocupacao)
-
         #TRATANDO DF LEITOS
         leitos = dfs[1].dropna().astype(str).values.tolist()
         new_leitos = []
@@ -132,10 +149,14 @@ def cleanner(dfs):
                     new_line.append(ll) 
             new_leitos.append(new_line) 
 
-        leitos = pd.DataFrame(new_leitos) 
-        leitos.columns = leitos_columns 
+        leitos = pd.DataFrame(new_leitos)
 
-        
+        if len(leitos.columns) > 13:
+            leitos.drop(columns=[4, 8, 12, 16], inplace=True)
+
+        leitos.columns = leitos_columns 
+        # print(leitos)
+
         porcentagem = lambda x,y: ((x/y)*100)
         leitos['uti adulto tx ocup'] = porcentagem(leitos['uti adulto ocup'].str.replace(".", "").astype(int), leitos['uti adulto exist'].str.replace(".", "").astype(int))
         leitos['enf adulto tx ocup'] = porcentagem(leitos['enf adulto ocup'].str.replace(".", "").astype(int), leitos['enf adulto exist'].str.replace(".", "").astype(int))
@@ -171,8 +192,7 @@ def cleanner(dfs):
         leitos['enf adulto tx ocup'] = porcentagem(leitos['enf adulto ocup'].str.replace(".", "").str.replace("%", "").astype(int), leitos['enf adulto exist'].str.replace(".", "").str.replace("%", "").astype(int))
         leitos['uti infantil tx ocup'] = porcentagem(leitos['uti infantil ocup'].str.replace(".", "").str.replace("%", "").astype(int), leitos['uti infantil exist'].str.replace(".", "").str.replace("%", "").astype(int))
         leitos['enf infantil tx ocup'] = porcentagem(leitos['enf infantil ocup'].str.replace(".", "").str.replace("%", "").astype(int), leitos['enf infantil exist'].str.replace(".", "").str.replace("%", "").astype(int))
-        
-
+    
         dfs[0] = leitos
         
     dfs = transform(dfs)
@@ -185,7 +205,7 @@ complements = ['_atualizado', '_1', '_0', '']
 texto = 'informe_epidemiologico'
 base_url = 'http://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{}/{}_{}{}.pdf'
 
-start_date = datetime(2020, 6, 23, 14, 0, 0)
+start_date = datetime(2020, 5, 6, 14, 0, 0)
 # start_date = datetime(2020, 6, 17, 14, 0, 0)
 
 hoje = datetime(2020, 6, 24, 19, 0, 0)
@@ -215,23 +235,21 @@ while start_date <= hoje:
     
     df = tabula.read_pdf(url, pages=[page], pandas_options={'header': None, 'dtype': str})
     
-    with pd.ExcelWriter('SESA_MESS{}.xlsx'.format(start_date.strftime('%d-%m'))) as writer:
-        for i in range(len(df)):
-            df[i].to_excel(writer, index=False, engine='xlsxwriter', encoding=' UTF-8', sheet_name='SESA_{}'.format(i))
+    # with pd.ExcelWriter('../../SESA_MESS_{}.xlsx'.format(start_date.strftime('%d-%m'))) as writer:
+    #     for i in range(len(df)):
+    #         df[i].to_excel(writer, index=False, engine='xlsxwriter', encoding=' UTF-8', sheet_name='SESA_{}'.format(i))
 
-    #PRINT MESS
-    # for i in range(len(df)):
-    #     print(df[i])
+
     df = cleanner(df)
 
     #PRINT CLEAN
     for i in range(len(df)):
-        print("O CARALHOOO")
+        # print("O CARALHOOO")
         print(df[i])
 
-    with pd.ExcelWriter('SESA_{}.xlsx'.format(start_date.strftime('%d-%m'))) as writer:
-        for i in range(len(df)):
-            df[i].to_excel(writer, index=False, engine='xlsxwriter', encoding=' UTF-8', sheet_name='SESA_{}'.format(i))
+    # with pd.ExcelWriter('SESA_{}.xlsx'.format(start_date.strftime('%d-%m'))) as writer:
+    #     for i in range(len(df)):
+    #         df[i].to_excel(writer, index=False, engine='xlsxwriter', encoding=' UTF-8', sheet_name='SESA_{}'.format(i))
 
     start_date += timedelta(days=1)
 
@@ -242,5 +260,5 @@ while start_date <= hoje:
 
 # /2020-05/informe_epidemiologico_06_05_2020_0.pdf start pg 4 - 1Table
 # /2020-05/informe_epidemiologico_19_05_2020_0.pdf start pg 5 - 1Table
-# /2020-06/informe_epidemiologico_09_06_2020_1.pdf start pg 5 - 2 Table
+# /2020-06/informe_epidemiologico_10_06_2020_1.pdf start pg 5 - 2 Table
 # /2020-06/INFORME_EPIDEMIOLOGICO_23_06_2020.pdf start pg 5 - 2Table
