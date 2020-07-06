67ccb504fcd0252cf08032851c78f5822aab54a0
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-07-06 13:40:54 -0300

    main

--- src/main.py
+++ src/main.py
@@ -8,35 +8,39 @@ from scripts.get_brio import get_brio_nacional, get_brio_cartorio
 from scripts.get_brapi import get_brapi_nacional, get_brapi_mundial
 
 
+if __name__ == '__main__':
+    insert_all()
+
+
 def insert_all():
     Session = sessionmaker(bind=engine_db())
     session = Session()
 
-    #Get_HDX_Mundial
+    # Get_HDX_Mundial
     with suppress(Exception):
         get_hdx_mundial.insert(session)
 
-    #Get_SESA_Sheets
+    # Get_SESA_Sheets
     with suppress(Exception):
         get_sesa_sheets.insert(session)
 
-    #Get_WCota_Leitos
+    # Get_WCota_Leitos
     with suppress(Exception):
         get_wcota_leitos.insert(session)
 
-    #Get_WCota_Nacional
+    # Get_WCota_Nacional
     with suppress(Exception):
         get_wcota_nacional.insert(session)
 
-    #Get_WCota_Suspeitos
+    # Get_WCota_Suspeitos
     with suppress(Exception):
         get_wcota_suspeitos.insert(session)
 
-    #Get_BRio_Nacional
+    # Get_BRio_Nacional
     with suppress(Exception):
         get_brio_nacional.insert(session)
 
-    #Get_BRio_Cartorio
+    # Get_BRio_Cartorio
     with suppress(Exception):
         get_brio_cartorio.insert(session)
 
