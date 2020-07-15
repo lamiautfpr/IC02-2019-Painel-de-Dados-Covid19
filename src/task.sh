3038d69d71cde4296454aa53b6c0ad7c8edcd374
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-07-15 17:39:59 -0300

    Update task.sh

--- src/task.sh
+++ src/task.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 source /var/projetos_lamia/2020/IC02-2019-Painel-de-Dados-Covid19/venv/bin/activate
 python3 /var/projetos_lamia/2020/IC02-2019-Painel-de-Dados-Covid19/src/main.py
 deactivate
