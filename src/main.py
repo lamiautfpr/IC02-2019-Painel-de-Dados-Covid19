0190d8dd7dfeb23fb201777c41aec1080811ea10
HugoJFreitas <hugojtdfreitas@gmail.com>
2020-08-04 14:17:00 -0300

    Insert get_sesa_time at main file

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from contextlib import suppress
 from database.engine_creator import engine_db
 from scripts.get_hdx import get_hdx_mundial
-from scripts.get_sesa import get_sesa_sheets, get_sesa_leitos, get_sesa_pr
+from scripts.get_sesa import get_sesa_sheets, get_sesa_leitos, get_sesa_pr, get_sesa_time_pr
 from scripts.get_wcota import get_wcota_leitos, get_wcota_nacional, get_wcota_suspeitos
 from scripts.get_brio import get_brio_nacional, get_brio_cartorio
 from scripts.get_brapi import get_brapi_nacional, get_brapi_mundial
@@ -22,6 +22,9 @@ def insert_all():
     with suppress(Exception):
         get_sesa_sheets.insert(session)
 
+    # Get_SESA_TIME_PR
+    with suppress(Exception):
+        get_sesa_time_pr.insert(session)
 
     # Get_SESA_PDF
     with suppress(Exception):
