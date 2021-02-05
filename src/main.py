59ddb381893e57d3b18d59ae89c05f4be4587f0f
HugoJFreitas <hugojtdfreitas@gmail.com>
2021-02-05 13:43:04 -0300

    get SESA e WCota Vacs in main function

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import sessionmaker
 from contextlib import suppress
 from database.engine_creator import engine_db
 from scripts.get_hdx import get_hdx_mundial
-from scripts.get_sesa import get_sesa_sheets, get_sesa_leitos, get_sesa_pr, get_sesa_time_pr
-from scripts.get_wcota import get_wcota_leitos, get_wcota_nacional, get_wcota_suspeitos
+from scripts.get_sesa import get_sesa_sheets, get_sesa_leitos, get_sesa_pr, get_sesa_time_pr, get_sesa_vacinas
+from scripts.get_wcota import get_wcota_leitos, get_wcota_nacional, get_wcota_suspeitos, get_wcota_vacinas
 from scripts.get_brio import get_brio_nacional, get_brio_cartorio
 from scripts.get_brapi import get_brapi_nacional, get_brapi_mundial
 #from scripts.get_SH import get_SH_sheets
@@ -37,6 +37,11 @@ def insert_all():
         get_sesa_pr.insert(session)
 
 
+    # # Get_SESA_Vacs
+    with suppress(Exception):
+        get_sesa_vacinas.insert(session)
+
+
     # # Get_WCota_Leitos
     with suppress(Exception):
         get_wcota_leitos.insert(session)
@@ -50,6 +55,10 @@ def insert_all():
     # # Get_WCota_Suspeitos
     with suppress(Exception):
         get_wcota_suspeitos.insert(session)
+
+    # # Get_WCota_Vacs
+    with suppress(Exception):
+        get_wcota_vacinas.insert(session)
 
 
     # # Get_BRio_Nacional
