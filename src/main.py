2a51639f69099f4ff0a3ca9791f8365a6169013d
HugoJFreitas <hugojtdfreitas@gmail.com>
2021-06-15 10:18:23 -0800

    main de teste

--- src/main.py
+++ src/main.py
@@ -12,14 +12,14 @@ def insert_all():
     Session = sessionmaker(bind=engine_db())
     session = Session()
 
-    get_sesa_vacinas.insert(session)
+    # get_sesa_vacinas.insert(session)
     # get_wcota_vacinas.insert(session)
 
     # # Get_HDX_Mundial
     # with suppress(Exception):
     #     get_hdx_mundial.insert(session)
 
-    # get_sesa_time_pr.insert(session)
+    get_sesa_time_pr.insert(session)
     # # Get_SESA_Sheets
     # with suppress(Exception):
     #     get_sesa_sheets.insert(session)
