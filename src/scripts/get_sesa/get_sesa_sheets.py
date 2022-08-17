b8f45b56775d61a797992d9d146f39aaf9168b7e
Nathalia Oliveira <nathaliavieiramota@gmail.com>
2022-08-17 22:09:02 +0200

    Ajustes nos scripts get_sesa_sheets e get_sesa_time para que voltem a executar atualizações automáticas + remoção de arquivos desnecessários

--- src/scripts/get_sesa/get_sesa_sheets.py
+++ src/scripts/get_sesa/get_sesa_sheets.py
@@ -5,29 +5,30 @@ from scripts.functions import now, format_date, previous_date
 
 def catcher():
     gid = [
-        '1593460334', '618041857', '1431072159', '1771168399',
-        '1317012264', '354728218', '355601818', '1342035615'
+        '312396411', '1684210468', '1984868632', '816983062',
+        '1368715649', '594742355', '1483238027', '1301182333'
     ]
     datasets = []
 
     date = now().date()
 
-    r = requests.get('http://www.saude.pr.gov.br/sites/default/'
-                    'arquivos_restritos/files/documento/2020-0{}/INFORME_EPIDEMIOLOGICO_{}.pdf'
-                    .format(date.month, format_date(0, date)))
-    r.raise_for_status
+    # r = requests.get('http://www.saude.pr.gov.br/sites/default/'
+    #                 'arquivos_restritos/files/documento/2020-0{}/INFORME_EPIDEMIOLOGICO_{}.pdf'
+    #                 .format(date.month, format_date(0, date)))
+    # r.raise_for_status
 
-    while not r.ok:
-        date = previous_date(date)
+    # while not r.ok:
+    #     date = previous_date(date)
 
-        r = requests.get('http://www.saude.pr.gov.br/sites/default/'
-                        'arquivos_restritos/files/documento/2020-0{}/INFORME_EPIDEMIOLOGICO_{}.pdf'
-                        .format(date.month, format_date(0, date)))
-        r.raise_for_status
+    #     r = requests.get('http://www.saude.pr.gov.br/sites/default/'
+    #                     'arquivos_restritos/files/documento/2020-0{}/INFORME_EPIDEMIOLOGICO_{}.pdf'
+    #                     .format(date.month, format_date(0, date)))
+    #     r.raise_for_status
 
     for gids in gid:
-        url = ("https://docs.google.com/spreadsheets/d/1mw17ZXJaRML5QKcZPACVE-"
-               "j7gJoqyv-TnOyG5ZCKINM/export?gid={}&format=csv".format(gids))
+        url = "https://docs.google.com/spreadsheets/d/1I2F3PRBv_9Al1rDSWH9eClHvWZ49op9AQFWqgNjkj70/export?gid={}&format=csv".format(gids)
+        # url = ("https://docs.google.com/spreadsheets/d/1mw17ZXJaRML5QKcZPACVE-"
+        #        "j7gJoqyv-TnOyG5ZCKINM/export?gid={}&format=csv".format(gids))
         dataset = pd.read_csv(url, encoding='utf-8',
                               engine='python', error_bad_lines=False)
         dataset.insert(len(dataset.columns), "insert_date", now())
